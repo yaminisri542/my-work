45fc855b6940c1d2e83b734f4a4c82d5bc05257c
yaminisri542 <147027612+yaminisri542@users.noreply.github.com>
2024-04-16 09:25:20 +0530

    Create week9a.js

---
+++ week9a.js
@@ -0,0 +1,7 @@
+var obj = { GoldPlatinum: "$1000" };
+var ob1 = { PinkGold: "$900" };
+var ob2 = { SilverTitanium: "$1500" };
+console.log("\nMobilecolor Price\n");
+console.log("\nGoldPlatinum:\t " + obj.GoldPlatinum + "\n");
+console.log("PinkGold:\t " + ob1.PinkGold + "\n");
+console.log(" SilverTitanium:\t" + ob2.SilverTitanium + "\n");
